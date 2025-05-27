a04167c9ba0ba9e4283cc1f2aae8b458d4087416
JayyPatel239 <int-jay.k.patel@outamationlabs.com>
2025-05-27 14:24:16 +0530

    Revert " chapter 1"
    This reverts commit b1238fbf1a6c808cdaf111e60b1b51c1f304dcba.

--- Chapter 1/chapter 1 ps/problem2.py
+++
@@ -1,1 +0,0 @@
-#Use REPL and print the table of 5 using it
--- Chapter 1/chapter 1 ps/problem3.py
+++
@@ -1,5 +0,0 @@
-#Install an external module and use it to perform an operation of your interest
-import pyttsx3
-engine = pyttsx3.init()
-engine.say("Virat Kohli is a GOAT!")
-engine.runAndWait()
