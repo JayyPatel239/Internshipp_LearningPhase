489ead62872aa9758a77dc5d5cdb8b1fb8971793
JayyPatel239 <int-jay.k.patel@outamationlabs.com>
2025-05-27 14:23:47 +0530

    Revert "Chapter 8 Functions and Recursion"
    This reverts commit 4723bd7a7341a903d0de1d2f6b7405f555062750.

--- chapter 8/03_func_with_argument.py
+++
@@ -1,5 +0,0 @@
-def goodday(name,ending):
-    print(f"Hello {name}, Good Morning!,{ending}")
-    return name
-name=goodday("Jay","Have a nice day!")
-print(name)
--- chapter 8/chapter 8 ps/03_prblm.py
+++
@@ -1,6 +0,0 @@
-
-# 3. How do you prevent a python print() function to print a new line at the end.
-print("A")
-print("B")
-print("C",end="") 
-print("D",end="")  
